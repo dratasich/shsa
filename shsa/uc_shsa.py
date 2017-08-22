327fbfa26b6ccd486f81b80cb14ebaf779779da6
Denise Ratasich <denise.ratasich@tuwien.ac.at>
2017-08-22 15:28:28 +0200

    enhance uc_shsa script

--- shsa/uc_shsa.py
+++ shsa/uc_shsa.py
@@ -17,33 +17,53 @@ parser.add_argument('-r', '--root', type=str, default="root",
 parser.add_argument('-c', '--config', type=str,
                     default="../config/shsamodel1.yaml",
                     help="SHSA model in a config file.")
+parser.add_argument('-d', '--dfs', action="store_true",
+                    help="Depth-first-search.")
+parser.add_argument('-g', '--greedy', action="store_true",
+                    help="Greedy search.")
+parser.add_argument('-p', '--pf', action="store_true",
+                    help="Search by particle filtering.")
 args = parser.parse_args()
 
+# additional input validation
 model = SHSAModel(configfile=args.config)
 assert args.root in model.nodes(), \
     "root '{}' is not part of the model".format(args.root)
 assert model.is_variable(args.root), "relation nodes cannot be substituted"
 
-print("DFS")
-engine = DepthFirstSearch(configfile=args.config)
-S = engine.substitute(args.root)
-print("- results:\n{}".format(S))
-print("- best: {}".format(S.best()))
+# print model
+model.write_dot("uc_shsa_model", 'pdf')
+print("config: {}".format(args.config))
 
-print("PF")
-engine = ParticleFilter(configfile=args.config)
-S = engine.substitute(args.root, best=0.76)
-print("- results:\n{}".format(S))
-print("- best: {}".format(S.best()))
+#
+# search
+#
 
-# substitution tree with highest utility
-engine.model.write_dot("uc_shsa_model", 'pdf')
-sbest = S.best()
-if sbest is not None:
-    sbest.write_dot("uc_shsa_substitution", 'pdf')
+S = None
 
-# print("Greedy")
-# engine = Greedy(configfile="../config/shsamodel1.yaml")
-# u, t = engine.substitute('root')
-# print("- utilities: " + str(u))
-# print("- trees: " + str(t))
+if args.dfs:
+    print("DFS")
+    engine = DepthFirstSearch(configfile=args.config)
+    S = engine.substitute(args.root)
+    print("- results:\n{}".format(S))
+    print("- best: {}".format(S.best()))
+
+if args.greedy:
+    print("Greedy")
+    engine = Greedy(configfile=args.config)
+    S = engine.substitute(args.root)
+    print("- results:\n{}".format(S))
+    print("- best: {}".format(S.best()))
+
+if args.pf:
+    print("PF")
+    engine = ParticleFilter(configfile=args.config)
+    S = engine.substitute(args.root)
+    print("- results:\n{}".format(S))
+    print("- best: {}".format(S.best()))
+
+# print substitution tree with highest utility
+if S is not None:
+    sbest = S.best()
+    if sbest is not None:
+        sbest.write_dot("uc_shsa_substitution", 'pdf')
