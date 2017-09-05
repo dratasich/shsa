1a1c232fc391929ef77dc5dcde46da979f394282
Denise Ratasich <denise.ratasich@tuwien.ac.at>
2017-09-05 15:33:04 +0200

    add testcases for best substitution

---
+++ shsa/test/test_engines_best.py
@@ -0,0 +1,46 @@
+import unittest
+import itertools
+
+from test.test_engines import SHSATestCase
+from engine.greedy import Greedy
+
+
+class SHSABestTestCase(SHSATestCase):
+    """Tests engines, that can return the globally optimal substitution."""
+
+    def setUp(self):
+        self.tcindex = {
+            'file': 0,
+            'root': 1,
+            'best': 2,
+        }
+        self.testcases = [
+            ("test/model1.yaml", 'root', {'t2'}),
+            ("test/model2.yaml", 'a', set()),
+            ("test/model3.yaml", 'a', {'r1'}),
+            ("test/model3.yaml", 'd', set()),
+            ("test/model4.yaml", 'a', {'r1', 'r2'}),
+            ("test/model_p1.yaml", 'a', {'r1', 'r2'}),
+            ("test/model_p2.yaml", 'a', {'r2'}),
+            ("test/model_p3.yaml", 'a', {'r1'}),
+        ]
+
+    def __check_results(self, S, no):
+        """Verify if the returned result is the best substitution."""
+        self.assertEqual(S[0].relations(),
+                         self.testcases[no][self.tcindex['best']],
+                         """best substitution wrong (TC{})""".format(no))
+
+    def test_greedy(self):
+        results = self.substitute_greedy()  # execute testcases
+        for i in range(len(results)):
+            self.__check_results(results[i], i)
+
+    def test_dfs(self):
+        results = self.substitute_dfs()  # execute testcases
+        for i in range(len(results)):
+            self.__check_results(results[i], i)
+
+
+if __name__ == '__main__':
+        unittest.main()
