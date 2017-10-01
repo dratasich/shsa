df2102d78b5805325a5a6f7583b52919e81f9d92
Denise Ratasich <denise.ratasich@tuwien.ac.at>
2017-10-01 13:57:21 +0200

    experiment 1 - add description to script

--- experiments/ex1.sh
+++ experiments/ex1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "ex1.sh\n" > ex1.yaml
+echo "experiment:" > ex1.yaml
+echo "  name: ex1.sh" >> ex1.yaml
+echo "  date: '$(date +'%Y-%m-%d %H:%M:%S')'" >> ex1.yaml
+echo >> ex1.yaml
 
 PYTHONPATH=../shsa python3 -O ../shsa/benchmark/ex1.py -n 100 -b 2 -d 2 >> ex1.yaml
 PYTHONPATH=../shsa python3 -O ../shsa/benchmark/ex1.py -n 100 -b 2 -d 4 >> ex1.yaml
