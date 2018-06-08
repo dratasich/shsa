772b5dfc2006a8001ff6d79ed8266aa99aed3f7f
Denise Ratasich <denise.ratasich@tuwien.ac.at>
2018-06-08 10:39:40 +0200

    minor changes
    * order fault types (max or sum can be used to generate a score)
    * minor comments
    * rename uc_monitor - specific to radar use case!

--- shsa/monitor/fault.py
+++ shsa/monitor/fault.py
@@ -11,12 +11,7 @@ class ItomFaultStatusType(IntEnum):
     """Types of fault status considered."""
     OK = 0
     """Check with other redundant information was ok."""
-    FAULTY = 1
-    """Check with at least two other redundant sources failed
-    (>3-itom-comparison).
-
-    """
-    UNDEFINED = 2
+    UNDEFINED = 1
     """We cannot make a point about the status of the information itom.
 
     For example:
@@ -24,10 +19,21 @@ class ItomFaultStatusType(IntEnum):
     - It is unclear which itom is faulty (2-itom-comparison).
 
     """
+    FAULTY = 2
+    """Check with at least two other redundant sources failed
+    (>3-itom-comparison).
+
+    """
 
 
 class FaultAgreement(object):
+    """Agrees about the fault status of a given list of values that should
+    match.
+
+    """
+
     def agree(self, values, error=1.0):
+        """Annotate the values with a fault status."""
         assert type(values) == list, "Parameter 'values' must be a list."
         # compare
         c = SimpleComparator()
@@ -35,7 +41,7 @@ class FaultAgreement(object):
         # 1-fault-tolerant
         faulty_idx = set()
         for i, m in enumerate(mismatches):
-            if m > 1:
+            if m > 1:  # at least two other values mismatch
                 faulty_idx.add(i)
         if len(faulty_idx) > 1:
             # more than one value incorrect, inconsistent
