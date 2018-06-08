a477f20ec2e6f7ee406e42f274cdb878c52cbb18
Denise Ratasich <denise.ratasich@tuwien.ac.at>
2018-06-08 10:46:45 +0200

    monitor radar sensors

---
+++ config/radar-tracking.py
@@ -0,0 +1,54 @@
+"""Utils for the relations of the radar tracking model."""
+
+def sign(p1, p2, p3):
+    """Returns the position (in terms of left and right) of p1 given the line
+    defined by p2 and p3.
+
+    https://www.gamedev.net/forums/topic/295943-is-this-a-better-point-in-triangle-test-2d/
+
+    """
+    return (p1[0] - p3[0]) * (p2[1] - p3[1]) \
+           - (p2[0] - p3[0]) * (p1[1] - p3[1])
+
+def is_point_in_triangle(pt, v1, v2, v3):
+    """Returns True if the 2D point pt is within the triangle defined by v1-3.
+
+    https://www.gamedev.net/forums/topic/295943-is-this-a-better-point-in-triangle-test-2d/
+
+    """
+    b1 = sign(pt, v1, v2) < 0.0
+    b2 = sign(pt, v2, v3) < 0.0
+    b3 = sign(pt, v3, v1) < 0.0
+    return ((b1 == b2) and (b2 == b3))
+
+def translate(point, translation):
+    """Translates a 2D point."""
+    px, py = point
+    tx, ty = translation
+    return [px + tx, py + ty]
+
+def rotate(origin, point, angle):
+    """Rotate a 2D point counterclockwise around the given origin.
+
+    origin -- 2D vector, origin of the rotation.
+    point -- 2D vector, point to rotate.
+    angle -- Angle in radians.
+
+    https://stackoverflow.com/questions/34372480/rotate-point-about-another-point-in-degrees-python?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+    """
+    import math
+    ox, oy = origin
+    px, py = point
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return [qx, qy]
+
+def in_field_of_view(point, location, heading, range, angle):
+    # approximate field of view with a triangle
+    import math
+    v1 = location
+    v2 = rotate(location, translate(v1, [range, 0]), heading - angle/2)
+    v3 = rotate(location, translate(v1, [range*math.sin(angle),
+                range*math.cos(angle)]), heading - angle/2)
+    return is_point_in_triangle(point, v1, v2, v3)
--- shsa/uc_radar_monitor.py
+++ shsa/uc_radar_monitor.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
-"""Test SHSA monitor."""
+#!/usr/bin/env python3
+"""Test radar monitor."""
 
 import argparse
 import numpy as np
+import itertools
 
 from monitor.shsamonitor import SHSAMonitor
 from monitor.fault import ItomFaultStatusType
@@ -12,9 +13,6 @@ from model.shsamodel import SHSAModel
 # parse optional config file
 parser = argparse.ArgumentParser(description="""Execute SHSA engines given a
 config file.""")
-parser.add_argument('-d', '--domain', type=str, default="s",
-                    help="""Common domain (variable of the SHSA model) where
-                    the itoms are compared against each other.""")
 parser.add_argument('-m', '--model', type=str,
                     default="../config/radar-tracking.yaml",
                     help="SHSA model in a config file.")
@@ -25,23 +23,176 @@ args = parser.parse_args()
 
 # create monitor
 model = SHSAModel(configfile=args.model)
-monitor = SHSAMonitor(model=model, domain=args.domain)
+x_monitor = SHSAMonitor(model=model, domain='x')
+y_monitor = SHSAMonitor(model=model, domain='y')
+# specify available itoms manually (do not match the csv)
 
+
+#
+# Data
+#
+# The data consists of rows of track observations per sensor and
+# timestamp. Each sensor creates a track for an observed vehicle. Track IDs per
+# sensors are unique (increase with every new detected vehicle). The position
+# (and velocity) included in each track observation are the itoms to
+# monitor.
+#
+# Additionally the monitor saves the last observation of a track observed by a
+# sensor.
 
 # open and parse csv
 print("Load data...")
-# use loadtxt to get 2D array right away
-# however, no names with this function
-#data = np.loadtxt(args.csv, delimiter=',', comments='%')
 data = np.genfromtxt(args.csv, delimiter=',', comments='%', names=True)
 header = data.dtype.names
 print("{} columns with names {}".format(len(data[0]), data.dtype.names))
 print("first row: {}".format(data[0]))
 
 
-# simulate monitoring over time: shift data into monitor
+#
+# Maintain track id pairs
+#
+# Pair the tracks corresponding to the same vehicle, when the vehicle changes
+# from one sensor s1 to sensor s2 (i.e., vanishes from the field of view of
+# sensor s1). To that end, the distance between all pairs of the vanished track
+# s1 to all tracks of s2 is calculated. The nearest track of s2 is selected if
+# its below a maximum offset (around vehicle size / lane width).
+
+def distance(track_a, track_b):
+    x = header.index('x')
+    y = header.index('y')
+    return (track_a[x] - track_b[x])**2 + (track_a[y] - track_b[y])**2
+
+def find_match(track, possible_tracks, max_off=6):
+    """Find a matching track id for the given id.
+
+    track -- The track for which to find a matching track from another sensor.
+    possible_tracks -- Tracks from another neighbor sensor.
+
+    Returns the id of the nearest track iff the distance is below the maximum
+    offset.
+
+    """
+    track_match = None
+    track_distance = max_off+1.0
+    # find best match (nearest track) in possible_tracks
+    for tid, t in possible_tracks.items():
+        d = distance(track, t)
+        if d < max_off and d < track_distance:
+            track_match = tid
+            track_distance = d
+    return track_match
+
+def update_id_pairs(s1, s2, track_id_pairs):
+    """Appends unmatched tracks in s1 to track_id_pairs."""
+    matched_ids = [pair[0] for pair in track_id_pairs]
+    for tid1, t1 in s1.items():
+        # try to find a near track for all unmatched tracks
+        if tid1 not in matched_ids:
+            tid2 = find_match(t1, s2)
+            # add new pair if successful
+            if tid2 is not None:
+                track_id_pairs.append([tid1, tid2])
+    return track_id_pairs
+
+def pair(s1, s2, track_id_pairs):
+    """Returns pairs of tracks, i.e., tracks that can be compared.
+
+    s1 -- all tracks of sensor 1.
+    s2 -- all tracks of sensor 2.
+    track_id_pairs -- ID pairs of tracks of s1 and s2.
+
+    """
+    pairs = []
+    # collect available ids of the sensor's tracks
+    for tid1, tid2 in track_id_pairs:
+        try:
+            t1 = s1[tid1]
+            t2 = s2[tid2]
+            pairs.append([t1, t2])
+        except:
+            # tracks may vanish from view, ignore
+            # (pair gets unused at some point)
+            pass
+    return pairs
+
+def check_pair(track_a, track_b):
+    x = header.index('x')
+    y = header.index('y')
+    itoms = {
+        'x': track_a[x],
+        'y': track_a[y],
+        'x_nbr': track_b[x],
+        'y_nbr': track_b[y],
+    }
+    status_x = x_monitor.monitor(itoms)
+    status_y = y_monitor.monitor(itoms)
+    status = {itom: max(status_x[itom], status_y[itom]) for itom in status_x.keys()}
+    return status
+
+# assume track ids are unique throughout the run (ids are not re-used when out
+# of range)
+track_id_pairs_s6 = []  # pairs s7 <--> s6
+track_id_pairs_s8 = []  # pairs s7 <--> s8
+
+def check(sensors, sensors_last):
+    """Check itoms in sensor 7 domain."""
+    if sensors is None:
+        return None
+    # default
+    status = None
+    # make the monitoring concrete here
+    try:
+        s6 = sensors[6]
+        s7 = sensors[7]
+        s8 = sensors[8]
+    except:
+        # if we don't have data from neighboring sensors, abort
+        return
+    # associate tracks from one sensor to the other
+    global track_id_pairs_s6
+    global track_id_pairs_s8
+    track_id_pairs_s6 = update_id_pairs(s7, s6, track_id_pairs_s6)
+    track_id_pairs_s8 = update_id_pairs(s7, s8, track_id_pairs_s8)
+    track_pairs_6 = pair(s7, s6, track_id_pairs_s6)
+    track_pairs_8 = pair(s7, s8, track_id_pairs_s8)
+    # check s7 against the neighbors
+    if not track_pairs_6 and not track_pairs_8:
+        print("no track pairs")
+        return
+    for track_under_test, track_redundant in track_pairs_6:
+        status = check_pair(track_under_test, track_redundant)
+    for track_under_test, track_redundant in track_pairs_8:
+        status = check_pair(track_under_test, track_redundant)
+    print(status)
+
+
+#
+# Simulate monitoring over time: shift data into monitor
+#
+
 print("Monitor...")
+t = -1.0
+sensors = None  # stores the current observation of the tracks per sensor
+sensors_last = {}  # keeps the last observation of a track per sensor
+status = None
 for row in data:
-    itoms = {name: row[name] for name in header}
-    status = monitor.monitor(itoms)
-    print(",".join([str(status[name]) for name in header]))
+    # gather itoms as long as its the same timestamp
+    if row['time'] != t:
+        # new timestamp
+        t = row['time']
+        # check the itoms gathered during the last period
+        check(sensors, sensors_last)
+        # reset current observations per sensor
+        sensors = {}
+    # create a list of tracks for each sensor
+    sid = row['sensor']
+    if sid not in sensors.keys():
+        sensors[sid] = {}  # key: sensor ID
+    if sid not in sensors_last.keys():
+        sensors_last[sid] = {}  # key: sensor ID
+    # prepare itoms
+    tid = row['track']
+    # save track observation
+    sensors[sid][tid] = row
+    # save last observation of a track (redundancy for forward estimation)
+    sensors_last[sid][tid] = row
